a2fdc0295c1e9b283dfc7055461a33153fcaa6c7
TSeydoux <72605796+TSeydoux@users.noreply.github.com>
2026-01-17 23:28:03 +0100

    Updated description
    I added a more complete description of the file content.

--- plots_LGATr.sh
+++ plots_LGATr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+### Script to create plots from 'plots.py'
+
 source /afs/cern.ch/work/t/thseydou/public/Miniconda/etc/profile.d/conda.sh    # Activates conda
 conda activate /afs/cern.ch/work/t/thseydou/public/Miniconda/envs/conda_ML     # Activates the specific conda environment
-cd /afs/cern.ch/work/t/thseydou/public/LGATr
-python plots.py                                                                # Runs the 'plots.py' script
+python /afs/cern.ch/work/t/thseydou/public/LGATr/ plots.py                     # Runs 'plots.py'
