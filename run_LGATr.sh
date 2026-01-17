17a9bc38952b8dd8aba567c1bb4d121bfaedc447
TSeydoux <72605796+TSeydoux@users.noreply.github.com>
2026-01-17 16:07:53 +0100

    Updated description
    I added a more complete description of the file content.

--- run_LGATr.sh
+++ run_LGATr.sh
@@ -2,4 +2,4 @@
 source /afs/cern.ch/work/t/thseydou/public/Miniconda/etc/profile.d/conda.sh    # Activates conda
 conda activate /afs/cern.ch/work/t/thseydou/public/Miniconda/envs/conda_ML     # Activates the specific conda environment
 cd /afs/cern.ch/work/t/thseydou/public/LGATr
-python run_LGATr.py
+python run_LGATr.py                                                            # Rusn the 'run_LGATr.py' script
