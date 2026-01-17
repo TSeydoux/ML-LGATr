74a9ee540f1a9030b0ea3a77a326408913ffb24e
TSeydoux <72605796+TSeydoux@users.noreply.github.com>
2026-01-17 23:29:04 +0100

    Updated description
    I added a more complete description of the file content.

--- data_prep.sh
+++ data_prep.sh
@@ -4,5 +4,4 @@
 
 source /afs/cern.ch/work/t/thseydou/public/Miniconda/etc/profile.d/conda.sh    # Activates conda
 conda activate /afs/cern.ch/work/t/thseydou/public/Miniconda/envs/conda_ML     # Activates the specific conda environment
-cd /afs/cern.ch/work/t/thseydou/public/LGATr
-python data_prep.py                                                            # Runs 'data_prep.py'
+python /afs/cern.ch/work/t/thseydou/public/LGATr/data_prep.py                  # Runs 'data_prep.py'
